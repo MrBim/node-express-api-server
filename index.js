d54ea549655875545205c8196f17bcfef1854036
bim williams <bim.williams@futurenet.com>
2019-08-22 08:43:46 +0100

    adding first test route

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const express = require('express')
-
+const PORT = process.env.PORT || 5000
 // init express
 const app = express()
 
 // create endpoints / rouote handlers
-app.get('', ( req, res ) => {
+app.get('/', ( req, res ) => {
     res.send('hello world')
 })
 
-app.listen(5000)
+app.get('/api/end-point', ( req, res ) => {
+    res.send({
+        blogOne:{
+            ttitle: "the blog",
+            body: "this is the body of the post, it has many words and is pleasnt to look at "
+        }
+    })
+})
+
+
+app.listen( PORT, () => console.log('server started on: port', PORT) )
